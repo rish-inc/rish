8332f6686acc48c3f162c27c062ebecc3e8bf13b
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-17 17:06:06 +0900

    functionsの接頭語を変更しました。

--- wp-content/themes/rishalice/functions.php
+++ wp-content/themes/rishalice/functions.php
@@ -1,5 +1,5 @@
 <?php
-function rishinc_readScript() {
-    wp_enqueue_style ( 'rishinc_css', get_theme_file_uri( '/css/style.css' ), array(), '1.0.0' );
+function rishaliceScript() {
+    wp_enqueue_style ( 'rishalice_css', get_theme_file_uri( '/css/style.css' ), array(), '1.0.0' );
 }
-add_action( 'wp_enqueue_scripts', 'rishinc_readScript' );
+add_action( 'wp_enqueue_scripts', 'rishaliceScript' );
