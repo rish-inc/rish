6230d2a49d783297298e297fc108658148804b87
YAT <yat@yat-net.com>
2022-06-17 17:24:40 +0900

    add-get-footer

--- wp-content/themes/rishalice/header.php
+++ wp-content/themes/rishalice/header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rish.inc</title>
-    <?php wp_head(); ?>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>rish.inc</title>
+	<?php wp_head(); ?>
 </head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
