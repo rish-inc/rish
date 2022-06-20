453127f46aac3be8fff04f481ab1d4456863e58a
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-20 14:46:27 +0900

    うさぎの顔をつくりcssの中身を色々変えました

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -1,10 +1,7 @@
 <?php get_header(); ?>
 
-<<<<<<< HEAD
-<p class="c-rabbit-body__title">テストやで</p>
-=======
-<p class="rabbit-body__title">テストやで</p>
-<a href="" class="menu__list">テストリンク</a>
->>>>>>> de3868b34522012751e6ee98f98efba7153b646e
+<p class="c-title-decoration-rabbit-face">顔つき</p>
+<p class="c-title-decoration-rabbit-body">テストやで</p>
+<a href="" class="c-hover--gmenu">テストリンク</a>
 
 <?php get_footer(); ?>
