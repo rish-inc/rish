f0ecfebb69688ad9bbf68c41b6342b2df0423f96
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-20 15:46:28 +0900

    タイトルラインを入れています

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -3,5 +3,6 @@
 <p class="c-title-decoration-rabbit-face">顔つき</p>
 <p class="c-title-decoration-rabbit-body">テストやで</p>
 <a href="" class="c-hover--gmenu">テストリンク</a>
+<h2 class="c-title-line">テストですよあああああああ</h2>
 
 <?php get_footer(); ?>
