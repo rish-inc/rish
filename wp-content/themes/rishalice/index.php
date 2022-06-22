26767dccd2b560f57eba53558b46c350b5f38d38
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-22 13:36:00 +0900

    凹みフレームを作りました

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -4,5 +4,6 @@
 <p class="c-decoration--rabbit--body">テストやで</p>
 <a href="" class="c-hover--gmenu">テストリンク</a>
 <h2 class="c-decoration--wing-under-line">テストですよあああああああ</h2>
+<div class="c-frame--dent">テストボックス</div>
 
 <?php get_footer(); ?>
