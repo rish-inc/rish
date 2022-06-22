f88ab306c66a3212b04f960df9009ec12d9b2b0e
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-22 14:57:54 +0900

    背景ストライプのコンポーネントを作りました

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -5,5 +5,6 @@
 <a href="" class="c-hover--gmenu">テストリンク</a>
 <h2 class="c-decoration--wing-line--under">テストですよあああああああ</h2>
 <div class="c-frame--dent">テストボックス</div>
+<div class="c-bg--stripe" style="width:400px; height:400px;">背景テスト</div>
 
 <?php get_footer(); ?>
