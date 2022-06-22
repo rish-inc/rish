0c9c9a004bc0f84aa79746e51985c115a041435a
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-06-22 13:47:28 +0900

    --wing-under-lineというclass名を--wing-line--underに変更

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -3,7 +3,7 @@
 <p class="c-decoration--rabbit--face">顔つき</p>
 <p class="c-decoration--rabbit--body">テストやで</p>
 <a href="" class="c-hover--gmenu">テストリンク</a>
-<h2 class="c-decoration--wing-under-line">テストですよあああああああ</h2>
+<h2 class="c-decoration--wing-line--under">テストですよあああああああ</h2>
 <div class="c-frame--dent">テストボックス</div>
 
 <?php get_footer(); ?>
