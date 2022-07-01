b71b6823819264eaf6c03f8fc440d63b74fae50b
sumitanoriko <syou@sumitanorikonoMacBook-Air.local>
2022-07-01 19:08:34 +0900

    色々作りました。

--- wp-content/themes/rishalice/index.php
+++ wp-content/themes/rishalice/index.php
@@ -4,17 +4,16 @@
 <p class="c-decoration--rabbit--body">テストやで</p>
 <a href="" class="c-hover--gmenu">テストリンク</a>
 <h2 class="c-decoration--wing-line--under">テストですよあああああああ</h2>
-<div class="c-frame--dent">テストボックス</div>
+<div class="c-hedding-bg--square-twodent">凹みテストボックス</div>
 <div class="c-bg--stripe" style="width:400px; height:400px;">背景テスト</div>
 <p class="c-bg--turquoise-green">背景グリーンにするテスト</p>
 <div class="c-frame--brackets">背景カギカッコにするコンポーネント</div>
 <button class="c-button--noise-line">これはボタンです。</button>
-<figure class="drop-shadow-bg--right" style="width:568px;"><img src="<?php echo get_theme_file_uri( '/images/top/solvable--alice-rabbit.jpg' ); ?>" alt=""></figure>
-<div class="bg--arrow--body">文章のテストです。1</div>  
-<div class="bg--arrow--body">文章のテストです。2</div>
-<div class="bg--arrow--body">文章のテストです。3</div>
-<div class="bg--arrow--tip"></div>  
-
-</div>
+<figure class="c-drop-shadow-bg--right" style="width:568px;"><img src="<?php echo get_theme_file_uri( '/images/top/solvable--alice-rabbit.jpg' ); ?>" alt=""></figure>
+<div class="c-bg-arrow--body">文章のテストです。1</div>  
+<div class="c-bg-arrow--body">文章のテストです。2</div>
+<div class="c-bg-arrow--body">文章のテストです。3</div>
+<div class="c-bg-arrow--tip"></div> 
+<div class="p-frame-circle">丸飾りフレーム用</div>
 
 <?php get_footer(); ?>
