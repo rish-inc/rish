a51083d51c01ad8ad72029d4340773c1382873ac
ueharamiu <ueharamiu66@gmail.com>
2022-08-08 18:08:12 +0900

    in the making form

---
+++ wp-content/themes/rishalice/components/template/contact.php
@@ -0,0 +1,27 @@
+<article class="p-form">
+	<div class="p-form__head c-decoration--wing-line--under">
+		<span class="c-decoration--english">Contact</span>
+		<h2 class="p-form__head__big">お問い合わせ</h2>
+	</div>
+	<div class="p-form__sentence">
+		<p class="p-form__sentence__text c-position">
+			お問合せについて、<br>
+			いざ思いや考えを伝えようとすると、どうすればうまく伝わるか考えてしまったり、<br>
+			伝えようとしてもうまく話せるか不安になりますよね。<br>
+			Web サイトやパンフレットの制作を依頼しようと思ってもどう話すか考えると<br>
+			問い合わせボタンすら押せなくなると思います。<br>
+		</p>
+		<p class="p-form__sentence__text c-position">
+			ですが、私達は受託制作のプロですので完璧な話でなくとも<br>
+			コミュニケーションを取りながら本当に伝えたい本質を探っていきます。<br>
+			まずはざっくりとしたお話でも構いませんのでお聞かせください。<br>
+		</p>
+		<p class="p-form__sentence__text c-position">
+			私達はその本質の中からあなたらしさが何であるか。<br>
+			そのあなたが表現したいことが何であるかを汲み取ってデザインをご提案させていただきます。<br>
+		</p>
+	</div>
+	<div class="p-form__inner">
+		<?php echo do_shortcode('[contact-form-7 id="16" title="お問い合わせ"]'); ?>
+	</div>
+</article>
--- wp-content/themes/rishalice/header.php
+++ wp-content/themes/rishalice/header.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>rish.inc</title>
+	<script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?b1QRw-8tAx4%3D&box=FgdHsKy~ab8%3D&aa=1&ab=2"></script>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
