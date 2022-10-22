b4ef5bba6212eb7865b28d4ba74166781d3e9dff
syokota <ueharamiu@rish.style>
2022-10-22 16:47:26 +0900

    私達が解決できる課題・ Rish の強み コーディング中

---
+++ wp-content/themes/rishalice/page-test.php
@@ -0,0 +1,77 @@
+<p class="c-decoration--rabbit--face">顔つき</p>
+<p class="c-decoration--rabbit--body">テストやで</p>
+<a href="" class="c-hover--gmenu">テストリンク</a>
+<h2 class="c-decoration--wing-line--under">テストですよあああああああ</h2>
+<div class="c-hedding-bg--square-twodent">凹みテストボックス</div>
+<!-- <div class="c-bg--stripe" style="width:400px; height:400px;">背景テスト</div> -->
+<p class="c-bg--turquoise-green">背景グリーンにするテスト</p>
+<figure class="c-drop-shadow-bg--right" style="width:568px;"><img src="<?php echo get_theme_file_uri( '/images/top/solvable--alice-rabbit.jpg' ); ?>" alt=""></figure>
+<div class="c-bg-arrow--body">文章のテストです。1</div>
+<div class="c-bg-arrow--body">文章のテストです。2</div>
+<div class="c-bg-arrow--body">文章のテストです。3</div>
+<div class="c-bg-arrow--tip"></div>
+<div class="p-frame-circle">丸飾りフレーム用</div>
+<div class="c-bg--diamond" style="width:400px; height:400px;">背景テストテストダイヤモンド</div>
+<p class="c-line--dot--bottom">ドットアンダーバー</p>
+<ul class="c-list-mark--round">
+	<li>テスト</li>
+	<li>のリスト</li>
+	<li>です</li>
+</ul>
+<p class="c-font--price">25<span class="c-font--price__complement">万円</span>~</p>
+<div class="c-frame--dent">
+	<div class="c-frame--dent__content">
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+		吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。吾輩は猫である。
+	</div>
+</div>
+<br>
+<br>
+<div class="c-frame--ornament--type-a">
+	私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。
+	私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。
+	私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。
+	私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。私は今英語を勉強しています。難しいです。
+</div>
+
+<br>
+<br>
+<div class="c-frame--ornament--type-b">
+	エンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディング
+	エンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディング
+	エンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディング
+	エンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディングエンパイヤーステイトビルディング
+</div>
+<br><br>
+<p class="c-require">必須</p>
+<div class="c-bg--spade" style="width:400px; height:400px;">背景テスト</div>
+<textarea  class="c-form--gold-round-corner"name="" id="" cols="30" rows="10">テストですよあああああああ</textarea>
+<div class="c-tilt--6" style="width:400px; height:400px; background: pink;">傾きテスト</div>
+<div class="c-tilt--10" style="width:400px; height:400px; background: blue;">傾きテスト</div>
+<div class="c-tilt--350" style="width:400px; height:400px; background: pink;">傾きテスト</div>
+<div class="c-tilt--354" style="width:400px; height:400px; background: blue;">傾きテスト</div>
+<p class="c-font--drop-shadow--black">ドロップシャドウ</p>
+
+<ul class="c-grid--3card">
+	<li>テスト</li>
+	<li>テスト</li>
+	<li>テストリンク</li>
+	<li>テストやでト</li>
+	<li>テストですよあああああああ</li>
+	<li>てれててのしと</li>
+</ul>
+<p>制作フロー<span class="c-decoration--english">Production flow</span></p>
+
+<div class="c-line--ornament">飾り線を出す</div>
+<br><br>
+<div class="c-frame--ornament--top-bottom">テストテスト</div>
+<br><br>
+<p class="c-decoration--biscuit">1</p>
+<br><br>
+<div class="c-decoration--diamond"><p class="c-decoration--diamond__number">1</p></div>
+
