df92ffc1a788509cddeb1faff359cc9ec44c23a5
Asami <aoi_ao726@yahoo.co.jp>
2022-12-10 19:41:35 +0900

    page-marketingのSP版完成

--- wp-content/themes/rishalice/page-marketing.php
+++ wp-content/themes/rishalice/page-marketing.php
@@ -3,7 +3,7 @@
 		<article class="c-inner c-content">
 			<section class="p-marketing--intro">
 				<figure class="p-marketing--intro__figure">
-					<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/01.png' ) );?>" alt="">
+					<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/01.png' ) );?>" alt="タイトル画像">
 					<figcaption class="p-marketing--intro__caption">
 						<i class="c-mark--megaphone"></i>
 						<h2 class="p-marketing--intro__caption__head">コンテンツ<br class="u-br--sp">マーケティング</h2>
@@ -14,7 +14,7 @@
 			</section>
 			<section class="p-marketing--problem">
 				<figure class="p-marketing--problem__figure">
-					<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/02.png' ) );?>" alt="">
+					<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/02.png' ) );?>" alt="問いかけ">
 					<figcaption class="p-marketing--problem__caption">
 						<h3 class="p-marketing--problem__caption__head">こんな悩みありませんか？</h3>
 						<ul class="p-marketing--problem__caption__list">
@@ -34,12 +34,12 @@
 				<p class=" p-marketing--solution__text">ユーザー目線で言えば欲しい商品を買う・知りたいことを問い合わせる・商品の詳しい資料を申し込む・サービスに会員登録することですね。</p>
 				<p class=" p-marketing--solution__text">当たり前の事を書いていますが、Web サイトで情報を伝えようとするととたんにどう書けばいいか分からなくなる方がかなり多いです。</p>
 				<p class=" p-marketing--solution__text">言葉で伝えるのが難しければ動画で解説しても構いませんし、イラストが得意な方は絵で伝えることもできます。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/03.png' ) );?>" alt="">
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/03.png' ) );?>" alt="イラストを描いたウサギ" class="u-mb--30">
 				<p class=" p-marketing--solution__text">しかしコンテンツを作るとなるととたんに何をどうすればいいのか分からなくなる方がかなり多いです。</p>
 				<p class=" p-marketing--solution__text">そもそも良いコンテンツとは一体何なのでしょうか。</p>
 				<p class=" p-marketing--solution__text">例えばあなたがオリジナルの化粧品を開発して Web サイトで販売するとします。 そこにはその商品のメリットやデメリット・成分表・金額を掲載し、製品が出来上がるまでの過程を紹介をするでしょう。 当然これらの情報はその化粧品を購入する人にとっては非常に価値あるものであり、購買を後押しするコンテンツであることは間違い有りません。</p>
-				<p class=" p-marketing--solution__text">ですが、それはその商品を知っている人や、他の商品と比較している人にとっては購入の後押しをするきっかけにはなりますが、その商品を知らない人にとっては購買の理由にはなかなか結びつきません。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/04.png' ) );?>" alt="">
+				<p class=" p-marketing--solution__text u-mb--62">ですが、それはその商品を知っている人や、他の商品と比較している人にとっては購入の後押しをするきっかけにはなりますが、その商品を知らない人にとっては購買の理由にはなかなか結びつきません。</p>
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/04.png' ) );?>" alt="メリットの例" class="u-mb--40">
 				<p class=" p-marketing--solution__text">あなたならどうでしょう。聞いたことがない商品の情報を見るだけでその商品を購入するでしょうか。</p>
 				<p class=" p-marketing--solution__text">おそらく答えはノーです。</p>
 				<p class=" p-marketing--solution__text">商品を購入を決定する場合、下記のような順序が少なからず存在します。</p>
@@ -49,22 +49,22 @@
 					<li>3.同様の製品よりも優れた情報を知る（価格や効果など）</li>
 				</ol>
 				<p class=" p-marketing--solution__text">コンテンツマーケティングは単に商品を知ってもらうために行うのではなく、その商品の価値をユーザーに知ってもらい、まずファンになってもらうこと。そして良いコンテンツとはこれらがしっかり伝達できるものを指します。</p>
-				<p class=" p-marketing--solution__text">化粧品で言えば試供品やサンプルを利用してもらうことや、すでに利用しているユーザーから商品に対する印象のフィードバックを得ることがコンテンツマーケティングに必要な部分になります。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/05.png' ) );?>" alt="">
+				<p class=" p-marketing--solution__text u-mb--66">化粧品で言えば試供品やサンプルを利用してもらうことや、すでに利用しているユーザーから商品に対する印象のフィードバックを得ることがコンテンツマーケティングに必要な部分になります。</p>
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/05.png' ) );?>" alt="サンプルプレゼントの例" class="u-mb--30">
 				<p class=" p-marketing--solution__text">そう考えるとコンテンツマーケティングで行うことは、商品の情報を掲載するだけでなく、ユーザーのサンプルや試供品の配布・口コミ情報の軽鎖なども含めたコンテンツなど、商品そのもの以外の部分も一緒に考えることが必要となってきます。</p>
 				<p class=" p-marketing--solution__text">そしてその情報をどうやって多くの人に知ってもらうか。そこからファンになってもらうかを考えて一つ一つコンテンツにし、それを周知していくことにあります。</p>
 				<p class=" p-marketing--solution__text">ところがいざコンテンツマーケティングのやり方を調べようと検索しても、出てくるのはコンテンツマーケティングの考え方や概念の解説ばかりで、結局何をすればいいのか分からないとなることが多いことでしょう。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/06.png' ) );?>" alt="">
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/06.png' ) );?>" alt="検索結果" class="u-mb--30">
 				<p class=" p-marketing--solution__text">なぜなら検索エンジンはあなたが調べたいキーワードに対して回答となるものを提案してきます。 コンテンツマーケティングのことが知りたいニーズにたいして、検索エンジンはそれに対する答えは導き出してくれますが、具体的に行う施策というのは上記で述べた試供品の配布や製品の口コミ情報の掲示なのでその中にはコンテンツマーケティングという言葉がそもそも出てきません。<br>そのためコンテンツマーケティングというキーワードで検索してもやり方自体が出てこないのです。<br>逆に言えば、コンテンツマーケティングのやり方を調べているユーザーに対して、それの回答になるコンテンツを用意することがコンテンツマーケティングとなり、ユーザーのニーズと用意したコンテンツがマッチすることになります。</p>
-				<p class=" p-marketing--solution__text">もっと分かりやすく言うと、あなたがユーザーとして化粧品の情報を知りたいときなんと調べるでしょうか。 「化粧品 口コミ」「化粧品 新作」「化粧品 サンプル」 といったより具体的なキーワードではないでしょうか。<br>ところがいざ自分がビジネスオーナーとして集客のことを考え出すと、多くの人が「サイト集客」「SNSマーケティング」「コンテンツマーケティング」といったキーワードで検索しはじめます。<br>何故なら化粧品を買うという意識から化粧品を売るという意識に変わっているためそこから派生するキーワードが脳内で別のものと紐付いてしまい、思考の先がどんどん別の方向に進んでしまうからです。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/07.png' ) );?>" alt="">
+				<p class=" p-marketing--solution__text u-mb--32">もっと分かりやすく言うと、あなたがユーザーとして化粧品の情報を知りたいときなんと調べるでしょうか。 「化粧品 口コミ」「化粧品 新作」「化粧品 サンプル」 といったより具体的なキーワードではないでしょうか。<br>ところがいざ自分がビジネスオーナーとして集客のことを考え出すと、多くの人が「サイト集客」「SNSマーケティング」「コンテンツマーケティング」といったキーワードで検索しはじめます。<br>何故なら化粧品を買うという意識から化粧品を売るという意識に変わっているためそこから派生するキーワードが脳内で別のものと紐付いてしまい、思考の先がどんどん別の方向に進んでしまうからです。</p>
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/07.png' ) );?>" alt="比較" class="u-mb--36">
 				<p class=" p-marketing--solution__text">こうなると同じ「化粧品」を検索するにしてもユーザーとして検索するときと、ビジネスオーナーになったときでは検索するキーワードの組み合わせも別のものになりユーザー目線が抜け落ちてしまい、結局何をすればいいのかがわからなくなっていきます。</p>
-				<p class=" p-marketing--solution__text">弊社のコンテンツマーケティングは、ビジネスオーナーが見えなくなってしまうユーザー目線をまず取り戻し、そこから集客へとつなぐ動線を共に考え構築するサービスです。</p>
-				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/08.png' ) );?>" alt="">
+				<p class=" p-marketing--solution__text u-mb--32">弊社のコンテンツマーケティングは、ビジネスオーナーが見えなくなってしまうユーザー目線をまず取り戻し、そこから集客へとつなぐ動線を共に考え構築するサービスです。</p>
+				<img src="<?php echo esc_url( get_theme_file_uri( '/images/marketing/08.png' ) );?>" alt="ユーザー目線を取り戻すウサギ" class="u-mb--36">
 				<p class=" p-marketing--solution__text">このページで説明した事をまず考え、必要な記事のライティング・ページ作成・写真撮影・イラストや漫画によるコンテンツ作成といったクリエイティブも含めたコンテンツマーケティングを提供しています。</p>
-				<p class=" p-marketing--solution__text">自社サイト運用のサポートを依頼したいしたい方はもちろん・話を聞いてみたいと思われた方でも是非お気軽に下記お問合せフォームからお問い合わせください。</p>
+				<p class=" p-marketing--solution__text u-mb--94">自社サイト運用のサポートを依頼したいしたい方はもちろん・話を聞いてみたいと思われた方でも是非お気軽に下記お問合せフォームからお問い合わせください。</p>
 			</section>
+			<?php get_template_part( 'components/template/contact' ); ?>
 		</article>
 	</main>
-<?php get_template_part( 'components/template/contact' ); ?>
 <?php get_footer(); ?>
