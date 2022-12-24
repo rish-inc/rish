b3f3d2cead9677a7a11133f2ed7cacbd8c2e3948
Asami <aoi_ao726@yahoo.co.jp>
2022-12-24 21:22:54 +0900

    site-productionのSP版作成中

--- wp-content/themes/rishalice/page-site-production.php
+++ wp-content/themes/rishalice/page-site-production.php
@@ -47,13 +47,13 @@
 							これらの役割を果たすにはただ長ければいいわけではなく、伝えるべき内容を1ページで完結させて伝える必要があります。<br>
 							そのため情報過多になってしまうものをどのようにわかりやすく解説するか？が重要であり、弊社では写真とテキストだけでなくイラストや漫画コンテンツを利用した施策が行なえます。
 						</dd>
-						<dt class="p-site-production--about--site-type__list__title"><i class="c-mark--rabbit-face"></i>ブログ・<br class="u-br--sp">メディアサイト</dt>
+						<dt class="p-site-production--about--site-type__list__title u-ai--sp--start"><i class="c-mark--rabbit-face"></i>ブログ・<br class="u-br--sp">メディアサイト</dt>
 						<dd class="p-site-production--about--site-type__list__description">
 							コーポレートサイトでは伝えきれない企業の日常や、インターネットショップの商品ページだけでは伝えきれない使い方や流行り廃りの情報など<br>
 							Web サイトを通じて情報を提供することで企業様のユーザーを増やしファンを作ることで企業様のブランディングが行えます。<br>
 							そのためにはどういった事を書かなければならないか？が大きなポイントとなるので、制作後の運用サポートも対応可能です
 						</dd>
-						<dt class="p-site-production--about--site-type__list__title"><i class="c-mark--rabbit-face"></i>予約システムなど<br class="u-br--sp">簡易Webシステム</dt>
+						<dt class="p-site-production--about--site-type__list__title u-ai--sp--start"><i class="c-mark--rabbit-face"></i>予約システムなど<br class="u-br--sp">簡易Webシステム</dt>
 						<dd class="p-site-production--about--site-type__list__description">
 							WordPress という CMS（コンテンツマネジメントシステム）を利用し、プラグインを活用することでご要望に沿ったシステムを導入できます。<br>
 							世界で最も利用されている WordPress はアメリカのホワイトハウスでも利用されるほどの人気のシステムは様々なニーズに答えることができ、企業様のサービスに多くの貢献をもたらせます。<br>
@@ -64,44 +64,58 @@
 			</article>
 			
 			<article class="p-site-production--production-flow">
-				<h3>制作フロー</h3>
+				<h3 class="p-site-production--production-flow__head">制作フロー</h3>
 				<span class="c-decoration--english">Production flow</span>
-				<dl class="c-frame--ornament--top-bottom">
-					<dt>01 設計フェーズ</dt>
-					<dd>
-						弊社では当事者になると当たり前になりすぎて見落としてしまう情報をヒアリングの段階で導き出し、それをコンセプトとしてコンテンツに落とし込み、企業様の世界観を大切にしながらユーザーに届けやすい情報設計を行いワイヤフレームを作成します。
-					</dd>
-					<dt>02 デザインフェーズ</dt>
-					<dd>
-						ワイヤーフレームを元にヒアリングで受け取った世界観をもとにサイト全体のビジュアル・UIをデザインします。<br>
-						訪問したユーザーに届けるビジュアルは企業様の世界観を伝えられるものか？<br>
-						訪問したユーザーが迷いなく知りたい情報にたどり着けるか？<br>
-						閲覧する情報は見やすく読みやすいか？<br>
-						様々な環境で閲覧しても情報が正しく伝わるか？<br>
-						あらゆる課題を解決しながら適切に情報を伝えるためのデザインを制作します。
-					</dd>
-					<dt>03 構築フェーズ</dt>
-					<dd>
-						デザインをブラウザで見れるよう専門の HTML や CSS、JavaScript といった言語を利用して Web サイトを構築します。<br>
-						小規模なサイトはもちろん、大規模なサイトの場合でも運用しやすい設計をもとにサイトを構築し、納品後に編集やページ追加依頼があっても1から作成するより短時間で対応可能。<br>
-						システム構築についても WordPress や Shopify といった CMS を活用しますので、フルスクラッチで1から開発するよりも低い予算で対応が可能です。
-					</dd>
-					<dt>04 サイトの解析ツール導入</dt>
-					<dd>
-						サイト運用のかなめとなるアナリティクスの設置やサーチコンソールの設定など、作ってから始まる Web サイトの状況を把握するにはアクセス解析が必須です。<br>
-						サイト制作の時点の設計はプランを立てた段階でしかなく、実際に稼働させて評価を行い、改善していかねばなりません。<br>
-						それにはデータあって初めて行えることで、これらのツールはそのデータを蓄積してくれます。<br>
-						サイトは作って終わるのではなく、そこからが始まりです。<br>
-						データを活用してよりよいサイトにしていきましょう。<br>
-						弊社では運用を一緒に行いデータの解析から改善提案・実装も承ることが可能です。
-					</dd>
-					<dt>05 サーバーへの納品</dt>
-					<dd>
-						作成するサイトタイプに合わせてレンタルサーバーやサーバースペックの選定を行い納品いたします。<br>
-						またトラフィック（サイトのアクセス数など）の高いサイトの場合レンタルサーバーだけでなく、VPS の構築や AWS を活用したサーバー構築の対応も可能。<br>
-						そしてセキュアな通信を証明する SSL の対応も対応可能です。
-					</dd>
-				</dl>
+				<div class="p-site-production--production-flow__wrapper c-bg-arrow--tip">
+					<dl class="p-site-production--production-flow__flow c-frame--ornament--top-bottom">
+						<div class="c-bg-arrow--body">
+							<dt class="p-site-production--production-flow__flow__title">
+								<span class="p-site-production--production-flow__flow__title__num c-decoration--biscuit">01</span>設計フェーズ
+							</dt>
+							<dd class="p-site-production--production-flow__flow__description">
+								弊社では当事者になると当たり前になりすぎて見落としてしまう情報をヒアリングの段階で導き出し、それをコンセプトとしてコンテンツに落とし込み、企業様の世界観を大切にしながらユーザーに届けやすい情報設計を行いワイヤフレームを作成します。
+							</dd>
+							<dt class="p-site-production--production-flow__flow__title">
+								<span class="p-site-production--production-flow__flow__title__num c-decoration--biscuit">02</span>デザインフェーズ
+							</dt>
+							<dd class="p-site-production--production-flow__flow__description">
+								ワイヤーフレームを元にヒアリングで受け取った世界観をもとにサイト全体のビジュアル・UIをデザインします。<br>
+								訪問したユーザーに届けるビジュアルは企業様の世界観を伝えられるものか？<br>
+								訪問したユーザーが迷いなく知りたい情報にたどり着けるか？<br>
+								閲覧する情報は見やすく読みやすいか？<br>
+								様々な環境で閲覧しても情報が正しく伝わるか？<br>
+								あらゆる課題を解決しながら適切に情報を伝えるためのデザインを制作します。
+							</dd>
+							<dt class="p-site-production--production-flow__flow__title">
+								<span class="p-site-production--production-flow__flow__title__num c-decoration--biscuit">03</span>構築フェーズ
+							</dt>
+							<dd class="p-site-production--production-flow__flow__description">
+								デザインをブラウザで見れるよう専門の HTML や CSS、JavaScript といった言語を利用して Web サイトを構築します。<br>
+								小規模なサイトはもちろん、大規模なサイトの場合でも運用しやすい設計をもとにサイトを構築し、納品後に編集やページ追加依頼があっても1から作成するより短時間で対応可能。<br>
+								システム構築についても WordPress や Shopify といった CMS を活用しますので、フルスクラッチで1から開発するよりも低い予算で対応が可能です。
+							</dd>
+							<dt class="p-site-production--production-flow__flow__title">
+								<span class="p-site-production--production-flow__flow__title__num c-decoration--biscuit">04</span>サイトの解析ツール導入
+							</dt>
+							<dd class="p-site-production--production-flow__flow__description">
+								サイト運用のかなめとなるアナリティクスの設置やサーチコンソールの設定など、作ってから始まる Web サイトの状況を把握するにはアクセス解析が必須です。<br>
+								サイト制作の時点の設計はプランを立てた段階でしかなく、実際に稼働させて評価を行い、改善していかねばなりません。<br>
+								それにはデータあって初めて行えることで、これらのツールはそのデータを蓄積してくれます。<br>
+								サイトは作って終わるのではなく、そこからが始まりです。<br>
+								データを活用してよりよいサイトにしていきましょう。<br>
+								弊社では運用を一緒に行いデータの解析から改善提案・実装も承ることが可能です。
+							</dd>
+							<dt class="p-site-production--production-flow__flow__title">
+								<span class="p-site-production--production-flow__flow__title__num c-decoration--biscuit">05</span>サーバーへの納品
+							</dt>
+							<dd class="p-site-production--production-flow__flow__description">
+								作成するサイトタイプに合わせてレンタルサーバーやサーバースペックの選定を行い納品いたします。<br>
+								またトラフィック（サイトのアクセス数など）の高いサイトの場合レンタルサーバーだけでなく、VPS の構築や AWS を活用したサーバー構築の対応も可能。<br>
+								そしてセキュアな通信を証明する SSL の対応も対応可能です。
+							</dd>
+						</div>
+					</dl>
+				</div>
 			</article>
 
 			<article class="p-site-production--price">
@@ -159,31 +173,33 @@
 			</article>
 
 			<article class="p-site-production--all-flow">
-				<h3>お問合せから公開までの流れ</h3>
+				<h3>お問合せから<br class="u-br--sp">公開までの流れ</h3>
 				<span class="c-decoration--english">Flow from contacting to website release</span>
-				<ol class="c-frame--ornament--top-bottom">
-					<li>
-						<span>1</span>弊社からご連絡後お打ち合わせさせていただき、御社のビジネスに関するヒアリングをはじめ、Web サイトの方向性などお伺いしながらサイト制作のお話をさせていただきます。またこのタイミングで契約書の取り交わしを行います。
-					</li>
-					<li>
-						<span>2</span>弊社でサイトマップを作成しますので、そちらご確認いただき必要な素材や外部サービスのアカウント作成、サーバーのご契約（弊社代行も可能）をお願いいたします。
-					</li>
-					<li>
-						<span>3</span>サイトマップをもとにサイト全体のコンテンツを掲載したワイヤーフレームを作成します。そちらをご確認いただきサイトの設計が問題ないかご確認いただきます。
-					</li>
-					<li>
-						<span>4</span>ワイヤーフレームをもとに Web サイトのデザインを行います。そちらをご確認いただき必要情報に漏れがないかなどご確認いただきます。
-					</li>
-					<li>
-						<span>5</span>確定したデザインをブラウザで表示させるための専用のプログラミングを行います。そちらをテストサーバーもしくは本番サーバーにアップロードしますので挙動をご確認いただきます。
-					</li>
-					<li>
-						<span>6</span>テストサーバーでの確認の場合は、問題がなければ本番サーバーにアップロードいたしますのでそちらの挙動を改めてご確認いただきます。
-					</li>
-					<li>
-						<span>7</span>テストサーバーでの確認の場合は、問題がなければ本番サーバーにアップロードいたしますのでそちらの挙動を改めてご確認いただきます。
-					</li>
-				</ol>
+				<div>
+					<ol class="c-frame--ornament--top-bottom">
+						<li>
+							<span>1</span>弊社からご連絡後お打ち合わせさせていただき、御社のビジネスに関するヒアリングをはじめ、Web サイトの方向性などお伺いしながらサイト制作のお話をさせていただきます。またこのタイミングで契約書の取り交わしを行います。
+						</li>
+						<li>
+							<span>2</span>弊社でサイトマップを作成しますので、そちらご確認いただき必要な素材や外部サービスのアカウント作成、サーバーのご契約（弊社代行も可能）をお願いいたします。
+						</li>
+						<li>
+							<span>3</span>サイトマップをもとにサイト全体のコンテンツを掲載したワイヤーフレームを作成します。そちらをご確認いただきサイトの設計が問題ないかご確認いただきます。
+						</li>
+						<li>
+							<span>4</span>ワイヤーフレームをもとに Web サイトのデザインを行います。そちらをご確認いただき必要情報に漏れがないかなどご確認いただきます。
+						</li>
+						<li>
+							<span>5</span>確定したデザインをブラウザで表示させるための専用のプログラミングを行います。そちらをテストサーバーもしくは本番サーバーにアップロードしますので挙動をご確認いただきます。
+						</li>
+						<li>
+							<span>6</span>テストサーバーでの確認の場合は、問題がなければ本番サーバーにアップロードいたしますのでそちらの挙動を改めてご確認いただきます。
+						</li>
+						<li>
+							<span>7</span>テストサーバーでの確認の場合は、問題がなければ本番サーバーにアップロードいたしますのでそちらの挙動を改めてご確認いただきます。
+						</li>
+					</ol>
+				</div>
 			</article>
 
 			<?php get_template_part( 'components/template/contact' ); ?>
