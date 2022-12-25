d893ef26a56481daba480a1383c4ba458268fc14
Asami <aoi_ao726@yahoo.co.jp>
2022-12-25 20:24:45 +0900

    SP版の料金表まで完成

--- wp-content/themes/rishalice/page-site-production.php
+++ wp-content/themes/rishalice/page-site-production.php
@@ -120,23 +120,25 @@
 			</article>
 
 			<article class="p-site-production--price">
-				<h3>料金表</h3>
+				<h3 class="p-site-production--price__head">料金表</h3>
 				<span class="c-decoration--english">price list</span>
-				<ul class="p-top-article--price__list">
-					<li class="p-top-article--price__list__item">
+				<ul class="p-site-production--price__list">
+					<li class="p-site-production--price__list__item">
 						<div class="p-card--price c-frame--dent">
+							<div class="p-card--price__out-inner"></div>
 							<dl class="p-card--price__inner c-bg--white">
 								<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
 									<span class="p-card--price__inner__plan__big">01.ミニマムプラン</span>
 									<span class="p-card--price__inner__plan__small">（コーポレートサイト向け）</span>
 								</dt>
 								<dd class="p-card--price__inner__number c-font--price">25<span class="c-font--price-complement">万円</span>〜</dd>
-								<dd class="p-card--price__inner__text c-font--normal">小規模なコーポレートサイト向けのプランです。<br>5ページほどのサイトになります。</dd>
+								<dd class="p-card--price__inner__text c-font--normal">小規模なコーポレートサイト向けのプランです。5ページほどのサイトになります。</dd>
 							</dl>
 						</div>
 					</li>
-					<li class="p-top-article--price__list__item">
+					<li class="p-site-production--price__list__item">
 						<div class="p-card--price c-frame--dent">
+							<div class="p-card--price__out-inner"></div>
 							<dl class="p-card--price__inner c-bg--white">
 								<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
 									<span class="p-card--price__inner__plan__big">02.ノーマルプラン</span>
@@ -147,20 +149,22 @@
 							</dl>
 						</div>
 					</li>
-					<li class="p-top-article--price__list__item">
+					<li class="p-site-production--price__list__item">
 						<div class="p-card--price c-frame--dent">
+							<div class="p-card--price__out-inner"></div>
 							<dl class="p-card--price__inner c-bg--white">
 								<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
 									<span class="p-card--price__inner__plan__big">03. CMS プラン</span>
-									<span class="p-card--price__inner__plan__small">（ブログ・ショッピングサイト・予約システム等）</span>
+									<span class="p-card--price__inner__plan__small c-font--tiny">（ブログ・ショッピングサイト・予約システム等）</span>
 								</dt>
 								<dd class="p-card--price__inner__number c-font--price">80<span class="c-font--price-complement">万円</span>〜</dd>
 								<dd class="p-card--price__inner__text c-font--normal">Web サイトにシステムを組み込み可能な CMS を利用したプランです。トップページ・一覧ページ・下層ページ・お問い合わせページ・商品ページなどを管理画面から登録運用できるサイトになります。</dd>
 							</dl>
 						</div>
 					</li>
-					<li class="p-top-article--price__list__item">
+					<li class="p-site-production--price__list__item">
 						<div class="p-card--price c-frame--dent">
+							<div class="p-card--price__out-inner"></div>
 							<dl class="p-card--price__inner c-bg--white">
 								<dt class="p-card--price__inner__plan-other c-hedding-bg--square-twodent">
 									<span class="p-card--price__inner__plan__big">04.コンテンツマーケティング</span>
