af27539f4a884f4c7322ef87f67ae05db2e50e67
syokota <ueharamiu@rish.style>
2023-01-06 18:18:27 +0900

    2023.01.06 commit

---
+++ wp-content/themes/rishalice/front-page.php
@@ -0,0 +1,168 @@
+<?php get_header(); ?>
+	<main class="l-main">
+		<article class="c-inner">
+			<div class="c-content">
+				<article class="p-top-article--mv">
+
+				</article>
+				<article class="p-top-article--about">
+					<h2 class="p-top-article--about__head">
+						<span class="p-top-article--about__head__text c-font--regular">
+							大阪市中央区で個人・中小企業様を中心に</span><br>
+						<span class="p-top-article--about__head__text c-font--regular">Web サイト（ホームページ）などを</span><br class="u-br--sp"><span class="p-top-article--about__head__text c-font--regular">制作している株式会社 Rish です</span>
+					</h2>
+					<div class="p-top-article--about__explanation c-font--middle c-frame--brackets">
+						<p class="p-top-article--about__explanation__words u-mb--30">Web サイト（一般的に言われるホームページ）を立ち上げたがうまく集客できない・売上が伸びないといった悩みは<br>見に来る人にとって必要なコンテンツが必要な場所に用意されていないことが原因です。</p>
+						<p class="p-top-article--about__explanation__words">トップページにこだわることも大切ですがユーザーはサイトにアクセスする時必ずトップページを経由するわけではありません。<br>「どこに何を用意するべきか」<br>それを共に考えながら Web サイトを制作・運用サポートを行うのが弊社の得意とする分野です。</p>
+					</div>
+				</article>
+				<article class="p-top-article--advantage">
+					<section class="p-top-article--advantage__parallel">
+						<figure class="c-drop-shadow-bg--right">
+							<img src="<?php echo esc_url( get_theme_file_uri( '/images/top/solvable--alice-rabbit.jpg' ) ); ?>" alt="">
+						</figure>
+						<div class="p-top-article--advantage__parallel__text">
+							<h3 class="p-top-article--advantage__parallel__text__head c-flex--center">
+								<i class="c-mark--rabbit-trumpet"></i>
+								私達が解決できる課題
+							</h3>
+							<ul class="p-top-article--advantage__parallel__text__list c-bg--spade">
+								<li class="c-dot--small c-font--middle">作って終わらない Web サイト・ホームページを作成して欲しい</li>
+								<li class="c-dot--small c-font--middle">ブランディングに必要なロゴ・グラフィック・写真・イラスト作成などお願いしたい</li>
+								<li class="c-dot--small c-font--middle">マーケティングのサポートをして欲しい</li>
+								<li class="c-dot--small c-font--middle">ブログ・メディアのアクセスアップサポートや記事執筆代行をお願いしたい</li>
+								<li class="c-dot--small c-font--middle">自分のプロフィール写真や従業員の写真、会社の写真撮影をお願いしたい</li>
+								<li class="c-dot--small c-font--middle">EC サイトの商品解説ページ作成や写真撮影を依頼したい</li>
+								<li class="c-dot--small c-font--middle">WordPress を使ったサイト運用を手伝って欲しい・機能追加して欲しい</li>
+								<li class="c-dot--small c-font--middle">WordPress のメンテナンスをお願いしたい</li>
+								<li class="c-dot--small c-font--middle">SEO に対する知識を身に着けたい</li>
+								<li class="c-dot--small c-font--middle">特定のキーワードを検索上位に表示したい</li>
+							</ul>
+						</div>
+					</section>
+					<section class="p-top-article--advantage__forte">
+						<h3 class="p-top-article--advantage__forte__head"> Rish の強み</h3>
+						<p class="c-font--general">弊社は代表がこれまで営業を行わずに個人事業主として Web サイト制作を生業にしてきました。</p>
+						<p class="c-font--general">これが成り立っていたのは個人で運営していた[ブログ](https://wp.yat-net.com/)や<br>[ツイッター](https://twitter.com/yat8823jp)などの SNS を活用した Web マーケティングにあります。</p>
+						<p class="c-font--general">現在は法人に切り替え、サイト運用やクリエイティブが行える社員や様々な分野のパートナーと共同しながら大手の Web サイト構築から個人の Web サイト制作まで幅広く対応が可能となりました。</p>
+						<p class="c-font--general">弊社の強みとしては納品してから始まる事業者様の運用サポートにあります。</p>
+						<p class="c-font--general">Web サイトを代行で作成してもそこからの運用は事業者様ご自身が集客していかなければなりません。</p>
+						<p class="c-font--general">しかし Web のことはまだまだ専門的な知識が必要なことが多く、日常の業務があるなかで Web サイト運用も行うのは非常に難しく納品した後もしばらく放置してしまうことも少なくありません。</p>
+						<p class="c-font--general">Web 担当者を雇用するには人件費をまだかけられない方もいらっしゃいます。</p>
+						<p class="c-font--general">弊社ではその運用をアウトソーシングという形でお受けし、事業者様がなるべく事業に専念してもらえるのが強みです。</p>
+					</section>
+					<a class="p-btn c-button--noise-line">お問い合わせはこちらから</a>
+				</article>
+				<article>
+					<div class="c-decoration--wing-line--under">
+						<span class="c-decoration--english">Works</span>
+						<h2>制作実績</h2>
+					</div>
+				</article>
+				<article class="p-top-article--price">
+					<div class="p-top-article--price__head c-decoration--wing-line--under">
+						<span class="c-decoration--english">Price</span>
+						<h2 class="c-font--big">サイト制作料金表</h2>
+					</div>
+					<ul class="p-top-article--price__list">
+						<li class="p-top-article--price__list__item">
+							<div class="p-card--price c-frame--dent">
+								<dl class="p-card--price__inner c-bg--white">
+									<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
+										<span class="p-card--price__inner__plan__big">01.ミニマムプラン</span>
+										<span class="p-card--price__inner__plan__small">（コーポレートサイト向け）</span>
+									</dt>
+									<dd class="p-card--price__inner__number c-font--price">25<span class="c-font--price-complement">万円</span>〜</dd>
+									<dd class="p-card--price__inner__text c-font--normal">小規模なコーポレートサイト向けのプランです。<br>5ページほどのサイトになります。</dd>
+								</dl>
+							</div>
+						</li>
+						<li class="p-top-article--price__list__item">
+							<div class="p-card--price c-frame--dent">
+								<dl class="p-card--price__inner c-bg--white">
+									<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
+										<span class="p-card--price__inner__plan__big">02.ノーマルプラン</span>
+										<span class="p-card--price__inner__plan__small">（中規模サイト向け）</span>
+									</dt>
+									<dd class="p-card--price__inner__number c-font--price">50<span class="c-font--price-complement">万円</span>〜</dd>
+									<dd class="p-card--price__inner__text c-font--normal">中規模サイト向けのプランです。20ページほどのサイトになります。</dd>
+								</dl>
+							</div>
+						</li>
+						<li class="p-top-article--price__list__item">
+							<div class="p-card--price c-frame--dent">
+								<dl class="p-card--price__inner c-bg--white">
+									<dt class="p-card--price__inner__plan c-hedding-bg--square-twodent">
+										<span class="p-card--price__inner__plan__big">03. CMS プラン</span>
+										<span class="p-card--price__inner__plan__small">（ブログ・ショッピングサイト・予約システム等）</span>
+									</dt>
+									<dd class="p-card--price__inner__number c-font--price">80<span class="c-font--price-complement">万円</span>〜</dd>
+									<dd class="p-card--price__inner__text c-font--normal">Web サイトにシステムを組み込み可能な CMS を利用したプランです。トップページ・一覧ページ・下層ページ・お問い合わせページ・商品ページなどを管理画面から登録運用できるサイトになります。</dd>
+								</dl>
+							</div>
+						</li>
+						<li class="p-top-article--price__list__item">
+							<div class="p-card--price c-frame--dent">
+								<dl class="p-card--price__inner c-bg--white">
+									<dt class="p-card--price__inner__plan-other c-hedding-bg--square-twodent">
+										<span class="p-card--price__inner__plan__big">04.コンテンツマーケティング</span>
+									</dt>
+									<dd class="p-card--price__inner__number c-font--price">150<span class="c-font--price-complement">万円</span>〜</dd>
+									<dd class="p-card--price__inner__text c-font--normal">集客の動線を設計し、サイトの構築を行うプランです。お客様の叶えたいビジネスに沿った構造と集客・運用を含めて制作いたします。</dd>
+								</dl>
+							</div>
+						</li>
+					</ul>
+					<a class="p-btn c-button--noise-line">サイト制作について</a>
+				</article>
+				<article class="p-top-article--blog">
+					<div class="p-top-article--blog__head c-decoration--wing-line--under">
+						<span class="c-decoration--english">Blog</span>
+						<h2 class="c-font--big">ブログ</h2>
+					</div>
+					<ul class="p-top-article--blog__list c-flex--parallel">
+					<?php
+						$query = new WP_Query(
+							array(
+								'posts_per_page' => 3, /* １ページあたりの投稿表示数 */
+							)
+						);
+					?>
+						<?php
+							if( $query -> have_posts() ):
+						?> <!--投稿があれば-->
+						<?php
+							while( $query -> have_posts() ):
+								$query -> the_post();
+						?> <!--投稿がある分だけ繰り返す-->
+						<li class="p-top-article--blog__list__item p-card">
+							<a class="p-card__link" href="<?php the_permalink(); ?>">
+								<figure class="p-card__link__caption">
+									<?php if(has_post_thumbnail()): ?>
+										<div class="p-card__link__caption__wrap">
+											<img class="p-card__link__caption__wrap__img" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+										</div>
+									<?php else: ?>
+										<div class="p-card__link__caption__wrap">
+											<img class="p-card__link__caption__wrap__img" src="<?php echo get_template_directory_uri(); ?>/images/blog/none-eye-catching.png" alt="">
+										</div>
+									<?php endif; ?>
+									<figcaption class="p-card__link__caption__text">
+										<span class="p-card__link__caption__text__day c-font--small"><?php echo esc_html( get_the_date( 'Y.m.d' ) ); ?></span>
+										<h3 class="p-card__link__caption__text__title c-font--medium"><?php the_title(); ?></h3>
+									</figcaption>
+								</figure>
+							</a>
+						</li>
+						<?php endwhile; ?>
+						<?php wp_reset_postdata(); ?> <!-- 投稿データをリセットする -->
+						<?php endif; ?>
+					</ul>
+				</article>
+
+
+				<?php get_template_part( 'components/template/contact' ); ?>
+			</div>
+		</article>
+	</main>
+<?php get_footer(); ?>
