f15aff56f0cd9fcdbfb16815c8de9f704f4274fc
Asami <aoi_ao726@yahoo.co.jp>
2023-02-06 21:22:11 +0900

    PC版サブメニュー、アニメーション作成

--- wp-content/themes/rishalice/src/scripts/main.js
+++ wp-content/themes/rishalice/src/scripts/main.js
@@ -1,9 +1,15 @@
 jQuery( function( $ ) {
 	const config = require( './config.js' );
 
-	// ハンバーガーボタンをクリックしたとき
+	// ハンバーガーボタンのクリック処理
     $(".p-header__button").click(function () {
         $(this).children("span").toggleClass('open');
 		$("#menu-header-menu").toggleClass('open');
+    });
+
+    // 親メニューのクリック処理
+    $(".menu-item-has-children").on('click', function() {
+        $(this).children("a").toggleClass('open');
+        $(this).children(".sub-menu").slideToggle('fast');
     });
 } );
