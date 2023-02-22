5db303581ad7873ec47ae5c16f709f02b1cecd88
Asami <aoi_ao726@yahoo.co.jp>
2023-02-22 23:19:57 +0900

    sp版 作成中

--- wp-content/themes/rishalice/footer.php
+++ wp-content/themes/rishalice/footer.php
@@ -1,18 +1,24 @@
+			<div class="p-go-to-top">
+				<span>Go to Top</span>
+			</div>
 			<footer class="p-footer">
-				<nav class="p-footer__menu">
+				<nav class="p-footer__menu c-frame--ornament--type-b">
 					<?php
 						wp_nav_menu( array (
 							'theme_location' => 'footer-menu',
 							'container'		 => false
 						) );
 					?>
+
+					<div class="p-footer__logoCopy">
+						<p class="p-footer__logoCopy__logo">
+							<a href="<?php echo esc_url(home_url('/')); ?>">
+								<img src="<?php echo esc_url( get_theme_file_uri( '/images/header-footer/logo_footer.svg' ) ); ?>" alt="ロゴ画像">
+							</a>
+						</p>
+						<p class="p-footer__logoCopy__copy"><small>&copy; Rish inc. All Rights Reserved.</small></p>
+					</div>
 				</nav>
-				<p class="p-footer__logo">
-					<a href="<?php echo esc_url(home_url('/')); ?>">
-						<img src="<?php echo esc_url( get_theme_file_uri( '/images/header-footer/logo_footer.svg' ) ); ?>" alt="ロゴ画像">
-					</a>
-				</p>
-				<p class="p-footer__copy"><small>&copy; Rish inc. All Rights Reserved.</small></p>
 			</footer>
 			<?php wp_footer(); ?>
 		</div>
