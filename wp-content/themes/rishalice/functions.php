eb263c7195e8e1e104c70546649772c2eaceea36
Asami <aoi_ao726@yahoo.co.jp>
2023-03-17 20:24:22 +0900

    PC版 ページネーション作成中

--- wp-content/themes/rishalice/functions.php
+++ wp-content/themes/rishalice/functions.php
@@ -36,3 +36,33 @@ function change_excerpt_more($more) {
 	return '…[…]';
 }
 add_filter('excerpt_more', 'change_excerpt_more');
+
+// デバイスによる表示切替
+function is_mobile() {
+    $useragents = array(
+        'iPhone',          // iPhone
+        'iPod',            // iPod touch
+        '^(?=.*Android)(?=.*Mobile)', // 1.5+ Android
+        'dream',           // Pre 1.5 Android
+        'CUPCAKE',         // 1.5+ Android
+        'blackberry9500',  // Storm
+        'blackberry9530',  // Storm
+        'blackberry9520',  // Storm v2
+        'blackberry9550',  // Storm v2
+        'blackberry9800',  // Torch
+        'webOS',           // Palm Pre Experimental
+        'incognito',       // Other iPhone browser
+        'webmate'          // Other iPhone browser
+    );
+    $pattern = '/'.implode('|', $useragents).'/i';
+    return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
+}
+
+// 投稿一覧に表示する記事数（PCとスマホで表示数を変える）
+// PCは管理画面で設定した記事数（9件表示）
+function mobile_posts_per_page( $query ) {
+	if ( ! is_admin() && is_mobile() && $query->is_main_query() ) {
+		$query->set( 'posts_per_page', 5 );  // スマホは5件表示
+	}
+}
+add_action( 'pre_get_posts', 'mobile_posts_per_page' );
