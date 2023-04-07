ab66e9edd1d098cc1bcf3b687711c03b72fab20e
Asami <aoi_ao726@yahoo.co.jp>
2023-04-07 22:54:26 +0900

    single.php 作成開始

--- wp-content/themes/rishalice/single.php
+++ wp-content/themes/rishalice/single.php
@@ -1,11 +1,55 @@
 <?php get_header(); ?>
-
-<?php
-	if( have_posts() ) :
-		while( have_posts() ) :
-			the_post(); ?>
-			<h2><?php the_title(); ?></h2>
-		<?php endwhile; ?>
-	<?php endif; ?>
-
+	<main class="l-main">
+		<article class="p-single c-inner c-content">
+			<div class="p-single__head c-decoration--wing-line--under">
+				<span class="c-decoration--english">Blog</span>
+				<h1 class="c-font--big">ブログ</h1>
+			</div>
+			<?php
+				if( have_posts() ) :
+					while( have_posts() ) : the_post(); ?>
+						<div class="p-single__contents">
+							<h2 class="p-single__contents__title"><?php the_title(); ?></h2>
+							<span class="p-single__contents__day"><?php echo esc_html( get_the_date( 'Y年m月d日') ); ?></span>
+							<?php
+                                $categories = get_the_category();
+                                if( $categories ){
+                                    echo '<ul class="p-single__contents__category-list">';
+                                    foreach( $categories as $category ){
+                                        echo '<li class="p-single__contents__category-list__item"><a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a></li>';
+                                    }
+                                    echo '</ul>';
+                                }
+                            ?>
+							<?php
+								$posttags = get_the_tags();
+								if( $posttags ){
+									echo '<div class="p-single__contents__tags">';
+									echo '<i class="c-mark--tag-icon"></i>';
+									echo '<ul class="p-single__tags">';
+									foreach ( $posttags as $tag ) {
+										echo '<li><a href="' . get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a></li>'; 
+									}
+									echo '</ul>';
+									echo '</div>';
+								}
+							?>
+						</div>
+						<figure class="p-single__contents__thumbnail">
+							<?php
+								if(has_post_thumbnail() ):
+									the_post_thumbnail(array(333, 254), array("alt" => get_the_title(), "class" => "p-single__contents__thumbnail__img"));
+								else:
+									?><img src="<?php echo get_template_directory_uri() ?>/images/blog/none-eye-catching.png" alt="画像はありません" class="p-single__contents__thumbnail--noimg"><?php
+								endif;
+							?>
+						</figure>
+						<article class="p-single__contents__content">
+							<?php the_content(); ?>
+						</article>
+					<?php endwhile; ?>
+			<?php endif; ?>
+			<?php get_template_part( 'components/template/contact' ); ?>
+		</article>
+	</main>
 <?php get_footer(); ?>
