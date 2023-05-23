b75ec629c98f54c744bf4e5f3a5c959809f43793
YAT <yat@yat-net.com>
2023-05-23 15:48:14 +0900

    consolelog

--- wp-content/themes/rdone2020/single.php
+++ wp-content/themes/rdone2020/single.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <?php
 	if( $post_type != "" ) {
+		echo "<script>console.log('" . $post_type . "');</script>";
 		get_template_part( 'components/parts/detail-portfolio', NULL,  get_post_type() );
 	}
 ?>
