0a63f8dd37f93f33b3d561bf7a721d2b3ff8a799
Asami <aoi_ao726@yahoo.co.jp>
2023-06-29 23:49:29 +0900

    制作実績一覧　作成中

--- wp-content/themes/rishalice/functions.php
+++ wp-content/themes/rishalice/functions.php
@@ -19,7 +19,7 @@ function rishaliceScript() {
 	wp_enqueue_style( 'rishalice_css', get_theme_file_uri( '/css/style.css' ), array(), '1.0.0' );
 	wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-3.6.3.min.js', '', '', true );
 	wp_enqueue_script( 'rishalice_js', get_theme_file_uri( '/js/bundle.js' ), array(), '1.0.0');
-    if( is_front_page() ) {
+    if( is_front_page() || is_page( 'works' ) ) {
 		wp_enqueue_script( 'masonry', get_theme_file_uri( '/js/masonry.min.js' ), array(), '4.2.2', true );
 		wp_enqueue_script( 'masonry-config', get_theme_file_uri( '/js/masonry-config.js' ), array( 'masonry' ), '4.2.2', true );
 	}
