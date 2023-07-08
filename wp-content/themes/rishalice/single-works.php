1a408b75d89b350841e493a5727e18a5fa2e866c
Asami <aoi_ao726@yahoo.co.jp>
2023-07-08 14:35:24 +0900

    制作実績詳細ページ　作成中

--- wp-content/themes/rishalice/single-works.php
+++ wp-content/themes/rishalice/single-works.php
@@ -6,11 +6,17 @@ Template Post Type: webdesign, print
 ?>
 <?php get_header(); ?>
 	<main class="l-main">
-		<article class="p-single c-inner c-content">
-			<div class="p-single__head c-decoration--wing-line--under">
+		<article class="p-single-works">
+			<div class="p-single-works__head c-decoration--wing-line--under">
 				<span class="c-decoration--english">Works</span>
 				<h1 class="c-font--big">制作実績</h1>
 			</div>
+			<!-- カスタム投稿の詳細ページなら専用ヘッダーを表示 -->
+			<?php
+				if ( is_singular( array( 'webdesign', 'print' ) ) ) :
+					get_template_part( 'components/template/header-single-works' );
+				endif;
+			?>
 			<?php
 				if( $post_type != "" ) {
 					get_template_part( 'components/template/detail-portfolio', NULL,  get_post_type() );
