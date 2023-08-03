2d604bfab32803bf170598615fd0710cc3d2286f
YAT <yat@yat-net.com>
2023-08-03 14:13:45 +0900

    add-sitelogo

--- wp-content/themes/rishalice/functions.php
+++ wp-content/themes/rishalice/functions.php
@@ -11,6 +11,7 @@ add_action( 'after_setup_theme', 'register_my_menus' );
 
 function custom_theme_support() {
 	add_theme_support( "post-thumbnails" );
+	add_theme_support( 'custom-logo' );
 }
 add_action( 'after_setup_theme', 'custom_theme_support' );
 
